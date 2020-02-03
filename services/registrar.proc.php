a271c3da8b34fd697be725a499a664d828e79dc6
jaimecll <jaimecgfg@gmail.com>
2020-02-03 18:16:18 +0100

    registro

--- services/registrar.proc.php
+++ services/registrar.proc.php
@@ -5,7 +5,7 @@
 
 if (isset($_REQUEST['nombre'])) {
 		$nombre=$_REQUEST['nombre'];
-
+	$aceptar=1;
 
 
 		if (isset($_REQUEST['password'])) {
@@ -17,20 +17,30 @@ if (isset($_REQUEST['nombre'])) {
 		$email=$_REQUEST['email'];
 
 
-
-		
-		$Insertar = mysqli_query($conn, "INSERT INTO tbl_usuario(nombre_user, password_user, email_user) VALUES ('$nombre','$pass','$email')");
+		$query = mysqli_query($conn, "SELECT email_user FROM tbl_usuario");
+		while($row = mysqli_fetch_array($query)){
+		if ($email == $row['email_user']) {
+			header("location:../registro.php");
+			exit;
+		}
+	}
+			
+		if ($aceptar==1) {
+			$Insertar = mysqli_query($conn, "INSERT INTO tbl_usuario(nombre_user, password_user, email_user) VALUES ('$nombre','$pass','$email')");
+		}
+		header("location:../index.php");
+		exit;
 }else{
-	header("location:../home.php");
+	header("location:../index.php");
 	exit;
 }
 
 }else{
-	header("location:../home.php");
+	header("location:../index.php");
 	exit;
 }
 }else{
-	header("location:../home.php");
+	header("location:../index.php");
 	exit;
 }
 	
