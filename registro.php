3ae35e79a7816f2aa368d28712602199be88a0be
jaimecll <jaimecgfg@gmail.com>
2020-02-05 20:26:10 +0100

    cosas pequeñas

--- registro.php
+++ registro.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 
     <head>
-        <title>Login</title>
+        <title>Registro</title>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="css/estilos.css">
+        <link rel="icon" type="image/png" href="img/logo/Logo-MyContacts.png">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <script src="https://kit.fontawesome.com/8876df5dfb.js"></script>
